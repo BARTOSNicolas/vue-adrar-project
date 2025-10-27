a6d8f01cf5dd9f76e4fd535a38f01e02c06879e1
BARTOSNicolas <73882719+BARTOSNicolas@users.noreply.github.com>
2025-10-27 14:52:55 +0100

    ExoRef avec le tableau

--- src/components/ExoRef.vue
+++ src/components/ExoRef.vue
@@ -1,11 +1,10 @@
 <script setup>
 import {ref} from "vue";
 
-const variable = "Valeur"
-const unLivre = ref("Les mémoire de Steven Seagal")
+const unLivre = ref("Les mémoires de Steven Seagal")
 const isOnline = ref(false)
 const unNombre = ref(1234567890)
-const unTableau = (ref["du texte", 99])
+const unTableau = ref(["Du texte", 99])
 const unObjet = ref({
   name: "COOL",
   tel: "060503032"
@@ -13,20 +12,78 @@ const unObjet = ref({
 const fonctionTest = ref(() => {
   return "Hello World"
 })
-
 </script>
 
 <template>
 <div class="container mx-auto p-8">
-  <p>{{ variable }}</p>
-  <p>{{ unLivre }}</p>
-  <p>{{ isOnline ? "Est en ligne" : "N'est pas en ligne" }}</p>
-  <p>{{ unNombre }}</p>
-  <p v-for="item in unTableau" :key="item">{{ item }}</p>
-  <p>{{ unObjet.name }}</p>
-  <p>{{ unObjet.tel }}</p>
-  <p>{{ fonctionTest() }}</p>
-  <p>{{ (() => "Hello World")() }}</p>
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+      <tr>
+        <th class="text-teal-300">Valeur</th>
+        <th class="text-teal-300">Variable</th>
+      </tr>
+      </thead>
+      <tbody>
+      <!-- row 1 -->
+      <tr>
+        <td>unLivre</td>
+        <td>{{ unLivre }}</td>
+      </tr>
+      <!-- row 2 -->
+      <tr>
+        <td>unNombre</td>
+        <td>{{ unNombre }}</td>
+      </tr>
+      <!-- row 3 -->
+      <tr>
+        <td>isOnline</td>
+        <td>{{ isOnline ? "Est en ligne" : "N'est pas en ligne" }}</td>
+      </tr>
+      <!-- row 4 -->
+      <tr>
+        <td>unTableau</td>
+        <td>{{ unTableau }}</td>
+      </tr>
+      <!-- row 5 -->
+      <tr>
+        <td>unTableau[0]</td>
+        <td>{{ unTableau[0] }}</td>
+      </tr>
+      <!-- row 6 -->
+      <tr>
+        <td>unObjet</td>
+        <td>{{ unObjet }}</td>
+      </tr>
+      <!-- row 7 -->
+      <tr>
+        <td>unObjet.name</td>
+        <td>{{ unObjet.name }}</td>
+      </tr>
+      <!-- row 8 -->
+      <tr>
+        <td>unObjet["name"]</td>
+        <td>{{ unObjet["name"] }}</td>
+      </tr>
+      <!-- row 9 -->
+      <tr>
+        <td>unObjet.tel</td>
+        <td>{{ unObjet.tel }}</td>
+      </tr>
+      <!-- row 10 -->
+      <tr>
+        <td>fonctionTest() variable</td>
+        <td>{{ fonctionTest()  }}</td>
+      </tr>
+      <!-- row 10 -->
+      <tr>
+        <td>fonctionTest() inline</td>
+        <td>{{ (() => "Hello World")() }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
 </template>
 
